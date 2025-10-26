d58c3fbdeb050b924933f4347827de223355f241
Kyle Uehlein <username@users.noreply.github.com>
2025-10-25 21:03:41 -0400

    remove harpoon configuration

--- lua/config/harpoon.lua
+++ lua/config/harpoon.lua
@@ -1,21 +1,28 @@
-local harpoon = require("harpoon")
+-- local harpoon = require("harpoon")
+--
+-- harpoon:setup()
+--
+-- vim.keymap.set("n", "<leader><leader>", function()
+-- 	harpoon.ui:toggle_quick_menu(harpoon:list())
+-- end)
+-- -- vim.keymap.set("n", "<leader>A", function() harpoon:list():prepend() end); -- hmmm....
+-- vim.keymap.set("n", "<leader>a", function()
+-- 	harpoon:list():add()
+-- end)
+--
+-- vim.keymap.set("n", "<C-n>", function()
+-- 	harpoon:list():next()
+-- end)
+-- vim.keymap.set("n", "<C-p>", function()
+-- 	harpoon:list():prev()
+-- end)
 
-harpoon:setup()
 
-vim.keymap.set("n", "<leader><leader>", function()
-	harpoon.ui:toggle_quick_menu(harpoon:list())
-end)
--- vim.keymap.set("n", "<leader>A", function() harpoon:list():prepend() end); -- hmmm....
-vim.keymap.set("n", "<leader>a", function()
-	harpoon:list():add()
-end)
 
-vim.keymap.set("n", "<C-n>", function()
-	harpoon:list():next()
-end)
-vim.keymap.set("n", "<C-p>", function()
-	harpoon:list():prev()
-end)
+
+
+
+
 
 -- -- Set <space>1..<space>5 be my shortcuts to moving to the files
 -- for _, idx in ipairs { 1, 2, 3, 4, 5 } do
