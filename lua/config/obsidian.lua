a94030a53dd1b1b4d8559d55cd30297fc8acd710
Kyle Uehlein <username@users.noreply.github.com>
2025-11-02 01:13:03 -0400

    debugging

--- lua/config/obsidian.lua
+++ lua/config/obsidian.lua
@@ -1,22 +1,31 @@
 --
 -- Obsidian configuration
 --
-require('obsidian').setup({
-  completion = {
-    nvim_cmp = false,
-    min_chars = 2,
-  },
-  daily_notes = {
-    folder = 'daily',
-  },
-  legacy_commands = false,
-  workspaces = {
-    {
-      name = 'notes',
-      path = os.getenv('HOME') .. '/Documents/obsidian-vault',
+
+local vault_path = vim.fn.expand('~/Documents/obsidian-vault')
+
+-- TODO: is there a better way to do this?
+-- `vault_path` probably wont exist on NixOS system build
+if vim.fn.isdirectory(vault_path) == 1 then
+  require('obsidian').setup({
+    completion = {
+      nvim_cmp = false,
+      min_chars = 2,
     },
-  },
-})
+    daily_notes = {
+      folder = 'daily',
+    },
+    legacy_commands = false,
+    workspaces = {
+      {
+        name = 'notes',
+        path = os.getenv('HOME') .. '/Documents/obsidian-vault',
+      },
+    },
+  })
+else
+  vim.notify('Obsidian vault not found at: ' .. vault_path, vim.log.levels.WARN)
+end
 
 vim.keymap.set('n', '<leader>on', '<cmd>Obsidian new<CR>', { desc = 'New note' })
 vim.keymap.set('n', '<leader>os', '<cmd>Obsidian search<CR>', { desc = 'Search notes' })
