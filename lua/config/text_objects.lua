0fff6e4439c50a2beef7ccf6c0b7cc34652db44a
Kyle Uehlein <username@users.noreply.github.com>
2026-05-26 18:57:25 -0400

    Fix mini.ai setup to include all default config values

--- lua/config/text_objects.lua
+++ lua/config/text_objects.lua
@@ -4,25 +4,43 @@
 local mini_ai = require('mini.ai')
 local layout_util = require('utils.layout')
 
--- Configure mini.ai based on current layout
-local function configure_textobjects()
-  local inside_key = layout_util.get_action_mapping(layout_util.ACTIONS.inside_textobj)
+-- Get the current inside key based on layout
+local inside_key = layout_util.get_action_mapping(layout_util.ACTIONS.inside_textobj)
 
-  mini_ai.setup({
-    -- Colemak DH uses 'r' for inside (mnemonic: "inneR")
-    -- QWERTY uses 'i' for inside (standard Vim)
-    mappings = {
-      inside = inside_key,
-      inside_next = inside_key .. 'n',
-      inside_last = inside_key .. 'l',
-    },
-  })
-end
-
--- Initial setup
-configure_textobjects()
+-- Initial setup with all defaults
+mini_ai.setup({
+  -- Colemak DH uses 'r' for inside (mnemonic: "inneR")
+  -- QWERTY uses 'i' for inside (standard Vim)
+  mappings = {
+    around = 'a',
+    inside = inside_key,
+    around_next = 'an',
+    inside_next = inside_key .. 'n',
+    around_last = 'al',
+    inside_last = inside_key .. 'l',
+    goto_left = 'g[',
+    goto_right = 'g]',
+  },
+  n_lines = 50,
+  search_method = 'cover_or_next',
+})
 
 -- Reconfigure when layout changes
 layout_util.on_layout_change(function()
-  configure_textobjects()
+  local new_inside_key = layout_util.get_action_mapping(layout_util.ACTIONS.inside_textobj)
+
+  mini_ai.setup({
+    mappings = {
+      around = 'a',
+      inside = new_inside_key,
+      around_next = 'an',
+      inside_next = new_inside_key .. 'n',
+      around_last = 'al',
+      inside_last = new_inside_key .. 'l',
+      goto_left = 'g[',
+      goto_right = 'g]',
+    },
+    n_lines = 50,
+    search_method = 'cover_or_next',
+  })
 end)
